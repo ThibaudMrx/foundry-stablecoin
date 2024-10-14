5237ef19fdcffb8c31cfcda7b551f8d05ad3e5ea
thibaudmrx <thibaudmerieux@gmail.com>
2024-10-14 19:28:23 +0200

    modules

---
+++ Makefile
@@ -0,0 +1,20 @@
+-include .env
+
+.PHONY: all test clean deploy fund help install snapshot format anvil zktest
+
+
+all: clean remove install
+
+# Clean the repo
+clean  :; forge clean
+
+# Remove modules
+remove :; 	rm -rf .gitmodules &&\
+			rm -rf .git/modules/* &&\
+			rm -rf lib &&\
+			touch .gitmodules &&\
+			git add . &&\
+ 			git commit -m "modules"
+
+install :; 	forge install forge install foundry-rs/forge-std@v1.8.2 --no-commit &&\
+ 			forge install openzeppelin/openzeppelin-contracts@v5.0.2 --no-commit
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,32 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title Decentralized Stable Coin
+ * @author Thibaud Merieux (implementing Patrick Collins tutorial)
+ * Collateral : ETH & BTC
+ * Minting: Algorithmic
+ * Burn: Algorithmic
+ * Peg: USD
+ */
